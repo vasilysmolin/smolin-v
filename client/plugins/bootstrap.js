1d0a317c0b27742bc080bdfd693b4e9eb422fe10
Vasily Smolin <vasily.smolin@spiti.ru>
2019-08-29 11:51:23 +0400

    first commit

--- client/plugins/bootstrap.js
+++
@@ -1,1 +0,0 @@
-import 'client/plugins/bootstrap'
--- client/plugins/vform.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import { HasError, AlertError, AlertSuccess } from 'client/plugins/vform'
-
-Vue.component(HasError.name, HasError)
-Vue.component(AlertError.name, AlertError)
-Vue.component(AlertSuccess.name, AlertSuccess)
